f056901b8619886b163fccf12b1493779b43a7e4
Austin Songer <a.songer@protonmail.com>
2021-04-29 01:52:57 +0000

    Updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-  #!/bin/bash
+#!/bin/bash
 # This script allows you to launch several images from this repository once they're built.
 # Make sure you add the `docker run` command in the header of the Dockerfile so the script can find it and execute it.
 # Use /tools/<Name of Tool>/Dockerfile
